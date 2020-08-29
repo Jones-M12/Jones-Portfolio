d7c86e6f527691b401540f47b45b16e1967fb86f
Jones-M <61522666+Jones-M12@users.noreply.github.com>
2020-08-28 20:05:25 -0400

    change style

--- careerfocus/src/components/SideNavBar.js
+++ careerfocus/src/components/SideNavBar.js
@@ -3,13 +3,13 @@ import React from "react";
 function SideNavBar() {
       return (
        <div> 
-        <div>
+        <div >
           <nav  href="#" className="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i /></nav>
           <aside id="colorlib-aside" role="complementary" className="border js-fullheight">
             <div className="text-center">
               <div className="author-img" style={{backgroundImage: 'url(images/profimage4.jpg)'}} />
-              <h1 id="colorlib-logo"><a href="index.html">Malesha Jones</a></h1>
-              <span className="position"><a href="#">Jr. Web Developer</a></span>
+              <h1 id="colorlib-logo" ><a href="index.html">Malesha Jones</a></h1>
+              <span className="position"><a href="#">Full Stack Web Developer</a></span>
             </div>
             <nav id="colorlib-main-menu" role="navigation" className="navbar">
               <div id="navbar" className="collapse">
