3dd93d6e10e6b7034d1ebdc2f5c50ca30f533b2e
Jones-M <61522666+Jones-M12@users.noreply.github.com>
2020-08-28 20:06:32 -0400

    setup skills component

---
+++ careerfocus/src/components/Skills.js
@@ -0,0 +1,144 @@
+import React from "react";
+// import ProjecetSelection from "./ProjectSelection";
+
+
+function Skills() {
+	return (
+		<section  classNameName="colorlib-skills" data-section="skills">
+			<div  classNameName="colorlib-narrow-content">
+				<div  classNameName="row">
+					<div  classNameName="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+						<span  classNameName="heading-meta">My Specialty</span>
+						<h2  classNameName="colorlib-heading animate-box">My Skills</h2>
+					</div>
+				</div>
+				<div  classNameName="row">
+					<div  classNameName="col-md-3 animate-box" data-animate-effect="fadeInLeft">
+						<div  className="services color-1">
+							<p><img src="./images/html5.png" width="75" height="75" ></img></p>
+							<br></br>
+							<h3>HTML</h3>
+						</div>
+					</div>
+					<div  className="col-md-3 animate-box" data-animate-effect="fadeInRight">
+						<div  className="services color-2">
+							<p><img src="./images/css.png" width="75" height="75" ></img></p>
+							<br></br>
+							<h3>CSS</h3>
+						</div>
+					</div>
+					<div  className="col-md-3 animate-box" data-animate-effect="fadeInTop">
+						<div  className="services color-3">
+							<p><img src="./images/javascript.png" width="75" height="75" ></img></p>
+							<br></br>
+							<h3>JavaScript</h3>
+						</div>
+					</div>
+					<div  className="col-md-3 animate-box" data-animate-effect="fadeInBottom">
+						<div  className="services color-4">
+							<p><img src="./images/jquery.png" width="75" height="75" ></img></p>
+							<br></br>
+							<h3>Jquery</h3>
+						</div>
+					</div>
+				</div>
+				<div  className="row">
+					<div  className="col-md-3 animate-box" data-animate-effect="fadeInLeft">
+						<div  className="services color-1">
+							<p><img src="./images/nodejs.png" width="75" height="75" ></img></p>
+							<br></br>
+							<h3>Node</h3>
+						</div>
+					</div>
+					<div  className="col-md-3 animate-box" data-animate-effect="fadeInRight">
+						<div  className="services color-2">
+							<p><img src="./images/express.png" width="75" height="75"></img></p>
+							<br></br>
+							<h3>Express</h3>
+						</div>
+					</div>
+					<div  className="col-md-3 animate-box" data-animate-effect="fadeInTop">
+						<div  className="services color-3">
+							<p><img src="./images/bootstrap.png" width="75" height="75"></img></p>
+							<br></br>
+							<h3>Bootstrap</h3>
+						</div>
+					</div>
+					<div  className="col-md-3 animate-box" data-animate-effect="fadeInBottom">
+						<div  className="services color-4">
+							<p><img src="./images/mongo.png" width="75" height="75" ></img></p>
+							<br></br>
+							<h3>Mongo</h3>
+						</div>
+					</div>
+				</div>
+				<div  className="row">
+					<div  className="col-md-3 animate-box" data-animate-effect="fadeInLeft">
+						<div  className="services color-1">
+							<p><img src="./images/mern.png" width="75" height="75"></img></p>
+							<br></br>
+							<h3>MERN</h3>
+						</div>
+					</div>
+					<div  className="col-md-3 animate-box" data-animate-effect="fadeInRight">
+						<div  className="services color-2">
+							<p><img src="./images/github.png" width="75" height="75" ></img></p>
+							<br></br>
+							<h3>Graphic Design</h3>
+						</div>
+					</div>
+					<div  className="col-md-3 animate-box" data-animate-effect="fadeInTop">
+						<div  className="services color-3">
+							<p><img src="./images/materialize.png" width="75" ></img></p>
+							<br></br>
+							<h3>Materialize</h3>
+						</div>
+					</div>
+					<div  className="col-md-3 animate-box" data-animate-effect="fadeInBottom">
+						<div  className="services color-4">
+							<p><img src="./images/heroku.png" width="75" height="75" ></img></p>
+							<br></br>
+							<h3>Heroku</h3>
+						</div>
+					</div>
+				</div>
+				<div  className="row">
+					<div  className="col-md-3 animate-box" data-animate-effect="fadeInLeft">
+						<div  className="services color-1">
+							<p><img src="./images/mysql.png" width="75" height="75"></img></p>
+							<br></br>
+							<h3>MySQL</h3>
+						</div>
+					</div>
+					<div  className="col-md-3 animate-box" data-animate-effect="fadeInRight">
+						<div  className="services color-2">
+							<p><img src="./images/npm.png" width="75" height="75" ></img></p>
+							<br></br>
+							<h3>NPM</h3>
+						</div>
+					</div>
+					<div  className="col-md-3 animate-box" data-animate-effect="fadeInTop">
+						<div  className="services color-3">
+							<p><img src="./images/react.png" width="75" ></img></p>
+							<br></br>
+							<h3>React</h3>
+						</div>
+					</div>
+					<div  className="col-md-3 animate-box" data-animate-effect="fadeInBottom">
+						<div  className="services color-4">
+							<p><img src="./images/bars.png" width="75" height="75" ></img></p>
+							<br></br>
+							<h3>Handlebars</h3>
+						</div>
+					</div>
+
+				</div>
+
+			</div>
+		</section>
+
+
+	);
+}
+
+export default Skills;
