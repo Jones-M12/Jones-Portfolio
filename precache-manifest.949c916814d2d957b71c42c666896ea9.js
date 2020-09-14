e3a157e8a261c048b43dcd609858964eac988c92
Jones-M <61522666+Jones-M12@users.noreply.github.com>
2020-09-14 16:47:14 -0400

    Updates

---
+++ precache-manifest.949c916814d2d957b71c42c666896ea9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a46457122740459020e363e18dac14dc",
+    "url": "/Jones-Portfolio/index.html"
+  },
+  {
+    "revision": "016245f170511311cba9",
+    "url": "/Jones-Portfolio/static/css/main.5429743f.chunk.css"
+  },
+  {
+    "revision": "ce73b7fb4b082575087b",
+    "url": "/Jones-Portfolio/static/js/2.393f3dbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Jones-Portfolio/static/js/2.393f3dbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "016245f170511311cba9",
+    "url": "/Jones-Portfolio/static/js/main.654d09e4.chunk.js"
+  },
+  {
+    "revision": "e8e862de672d67857fbc",
+    "url": "/Jones-Portfolio/static/js/runtime-main.c308a2a8.js"
+  }
+]);
